2507cf03fdc1f2d301c0bd42bf0dacc7f29e006a
minakshi <minakshibisen04@gmail.com>
2025-04-08 12:19:26 +0530

    TemporaryFavoriteItem List add  in state

--- lib/bloc/favoritelist/favorite_list_state.dart
+++ lib/bloc/favoritelist/favorite_list_state.dart
@@ -4,17 +4,19 @@ enum ListStatus { loading, success, failure }
 
 class FavoriteListState extends Equatable {
   final List<FavoriteModelList> favoriteItemList;
+  final List<FavoriteModelList> temporaryFavoriteItemList;
   final ListStatus listStatus;
 
   const FavoriteListState(
-      {this.favoriteItemList = const [], this.listStatus = ListStatus.loading});
+      {this.favoriteItemList = const [], this.temporaryFavoriteItemList = const [],this.listStatus = ListStatus.loading});
 
-  FavoriteListState copyWith({List<FavoriteModelList>? favoriteItemList,ListStatus? listStatus}){
+  FavoriteListState copyWith({List<FavoriteModelList>? favoriteItemList,List<FavoriteModelList>? temporaryFavoriteItemList,ListStatus? listStatus}){
     return FavoriteListState(
     favoriteItemList: favoriteItemList ?? this.favoriteItemList,
+        temporaryFavoriteItemList: temporaryFavoriteItemList ?? this.temporaryFavoriteItemList,
       listStatus: listStatus ?? this.listStatus
     );
 }
   @override
-  List<Object?> get props => [favoriteItemList,listStatus];
+  List<Object?> get props => [favoriteItemList,listStatus,temporaryFavoriteItemList];
 }
